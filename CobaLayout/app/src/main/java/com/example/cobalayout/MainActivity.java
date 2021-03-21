5b8d13a56f86ca26986c825533485a8bcf69f7d8
KanaFarira <kanafarira@gmail.com>
2021-03-21 15:54:53 +0700

    perbaikan

--- CobaLayout/app/src/main/java/com/example/cobalayout/MainActivity.java
+++ CobaLayout/app/src/main/java/com/example/cobalayout/MainActivity.java
@@ -13,8 +13,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.relative_layout);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.relative_layout);
         //setContentView(R.layout.constraint_layout);
         //setContentView(R.layout.frame_layout);
         //setContentView(R.layout.tabel_layout);
